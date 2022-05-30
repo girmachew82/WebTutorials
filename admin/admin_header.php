2bdecb1d53024bf7b0df8978f8d5867cd797aa50
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2022-05-30 18:15:35 +0300

    CSS

--- admin/admin_header.php
+++ admin/admin_header.php
@@ -1,6 +1,35 @@
 <?php
 session_start();
 ?>
+<style>
+    /* unvisited link */
+a:link {
+  color: #000000;
+}
+
+/* visited link */
+a:visited {
+  color: #00FF00;
+}
+
+/* mouse over link */
+a:hover {
+  color: #FF00FF;
+}
+
+/* selected link */
+a:active {
+  color: #0000FF;
+}
+a {
+    text-decoration: none;
+    padding: 10px;
+    color: #FFFFFF;
+}
+nav {
+    background-color: #123456;
+}
+</style>
 <header>
 <nav>
 <?php
--- index.php
+++ index.php
@@ -7,84 +7,120 @@
 ul {
 list-style-type: circle;
 }
+/* style="background-color :green; color:white;font-family:Georgia, serif;"*/
+body {
+	margin-left: 40px;
+	background-color :#E8A87C; 
+	color:white;
+	font-family:Georgia, serif;
+}
+a:link {
+  color: #FF22AA;
+}
+
+/* visited link */
+a:visited {
+  color: #00FF00;
+}
+
+/* mouse over link */
+a:hover {
+  color: #44AABB;
+}
+
+/* selected link */
+a:active {
+  color: #0000FF;
+}
+a {
+    text-decoration: none;
+    padding: 10px;
+    color: #FFFFFF !important;
+}
+nav {
+    background-color: #123456;
+}
+footer{
+	clear: both;
+	height: 22%;
+	background-color: #123456;
+}
+
+#article1 {
+	float: left;
+	background-color: #e18c27;
+    font-family: fantasy
+}
+#article2 {
+	float: right;
+}
 </style>
    </head>
+   <body >
+		<nav>
 
-   <body style="background-color :green; color:white;font-family:Georgia, serif;">
-<nav>
+			<img src="images/dbuicon.png" alt="Logo" width="100" height="50">
+			<a href="index.html">Home</a>	
+			<a href="about.html">About </a>
+			<a href="help.html">Help</a>
+			<a href="contact.html">Contact</a>
+			<a href="login.html">Login</a>
 
-	<img src="images/dbuicon.png" alt="Logo" width="100" height="50">
-	<a href="index.html">Home</a>	
-   <a href="about.html">About </a>
-   <a href="help.html">Help</a>
-   <a href="contact.html">Contact</a>
-   <a href="login.html">Login</a>
+		</nav>
 
-</nav>
-<input type="file" accept=
-" application/pdf">
-<device type="media" onchange="update(this.data)"></device>
-<video autoplay></video>
-<script>
-  function update(stream) {
-    document.querySelector('video').src = stream.url;
-  }
-</script>
 	<header> DBU E-learning System </header>		
-<section>
-	<article>
-
-
-<!-- HTML 5 tutorils contents goes here -->
-<P>HTML is new Markup language </p>
+	<section>
+	<article id="article1">
+	<!-- HTML 5 tutorils contents goes here -->
+    <P>HTML is new Markup language </p>
 	<p>There are new feartures in this language such as Geolocation, drag and drop, API</p>
 	<p>In HTMl elements or tags can be catagorize into two </p>
 	<p>block level</p>
-		<p> some of <a href="blocklevel.html" target="_top ">block level elements </a></p><a href="#pre">Pre</a>
+	<p> some of <a href="blocklevel.html" target="_top ">block level elements </a></p><a href="#pre">Pre</a>
 			<ul >
 			<li>paragraph</li>
 			<li>heading</li>
 			<li>list</li>
 			<li>table</li>
 			</ul>
-	<hr>
-	the above lists are presented by default unordered list element and if you want change the default list bullete add attrubute at open tag of unordered list  
-	<hr>
+			<hr>
+			the above lists are presented by default unordered list element and if you want change the default list bullete add attrubute at open tag of unordered list  
+			<hr>
 			<ul >
 			<li>paragraph</li>
-										 <li>heading</li>
+			<li>heading</li>
 			<li>list</li>
 			<li>table</li>
 			</ul>
-	<hr>
-	<p>Inline</p>
+			<hr>
+	     <p>Inline</p>
 		<ol>
 		<li>italic</li>
-					  <li>bold</li>
+		 <li>bold</li>
 		<li>underline	</li>
 		</ol>
 		<ol type="A">
 		<li>italic</li>
-					  <li>bold</li>
+		 <li>bold</li>
 		<li>underline	</li>
 		</ol>
 		<ol type="a">
 		<li>italic</li>
-					  <li>bold</li>
+		<li>bold</li>
 		<li>underline	</li>
 		</ol>
 		<ol type="I">
 		<li>italic</li>
-					  <li>bold</li>
+		<li>bold</li>
 		<li>underline	</li>
 		</ol>
 		<ol type="i">
 		<li>italic</li>
-					  <li>bold</li>
+		 <li>bold</li>
 		<li>underline	</li>
 		</ol>
 	</article>
-	<article>
+	<article id="article1">
 	<hr>
 	<p>Inline elements
 		<i>italic </i> <b>bold</b> &nbsp;      &nbsp;  &nbsp;  &nbsp;                     <strong>strong</strong>
@@ -94,7 +130,7 @@ list-style-type: circle;
 	<del>Delte</del>
 	<ins>Insert</ins>
 	</p>
-</article>
+
 	<hr>
 	Headings
 	<h1>Heading</h1>
@@ -105,9 +141,9 @@ list-style-type: circle;
 	<h6>Heading</h6>
 	<p>Inline elements
 		<i>italic </i> <b>bold</b> <br>
-						<strong>strong</strong><br>
-						X<sup>2</sup><br>
-					   H<sub>2</sub>O <br>
+		<strong>strong</strong><br>
+		X<sup>2</sup><br>
+		H<sub>2</sub>O <br>
 	 <em>emphasis</em><br>
 	<u>Underline</u> <br><small>Small</small>
 	This inline tags also called formting tags
@@ -123,6 +159,7 @@ list-style-type: circle;
 	</pre>
 	<hr>
 	<img src="images/html5log.png" width="200" height="200" alt="" >
+	</article>
 	
 		</section>
 <footer>
